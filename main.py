4021974de52fb326483325a3ed88c5df0d79b26c
Marcelo Böemeke da Silveira <marcelo.boemeke@gmail.com>
2023-09-29 13:55:00 -0300

    Cria o arquivo principal do projeto

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Goals Tracking")
+
+st.markdown("""
+# Goals Tracking
+This is an app that allows you to track your personal goals in an intuitive and visual way
+""")
